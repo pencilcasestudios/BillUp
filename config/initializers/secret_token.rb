72f79b3e290ce6c7351af5a9328ccc7d910ffba9
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-09-17 12:22:47 +0200

    Initial project import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BillUp::Application.config.secret_token = '6fc933c056136c218941f025726240c97f8308aefabf6f7866a3c15e8fa18cc5e7d8dd596fa7f5b7ef3723153b174dfe5cac6d8ee99cbf5661381c45d82338ff'
