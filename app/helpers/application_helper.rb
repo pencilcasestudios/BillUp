ee31b6d179c1a249bebb6b202f242477cd3ea6d6
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-09-20 19:19:34 +0200

    Helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def body_id
+    controller.controller_name
+  end
+
+  def new_or_create_action?
+    (controller.action_name == "new") || (controller.action_name == "create")
+  end 
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
 end
