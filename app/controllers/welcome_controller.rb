c6bf398a844fdd1d72ea7b50a7f96e9db6c8ef36
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-09-23 19:13:01 +0200

    Different home page depending on whether a user is logged in or not.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+    if user_signed_in?
+      render :signed_in_home
+    else
+    end
   end
 
   def pricing
+  end
+
+  def signed_in_home
   end
 
   def terms
