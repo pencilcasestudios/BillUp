8443f90b7e9761ac9061a3ca4fb5fa1016c38895
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-09-27 20:44:59 +0200

    Organisation and Membership migrations.

--- db/migrate/20110924061752_create_organisations.rb
+++ db/migrate/20110924061752_create_organisations.rb
@@ -1,6 +1,8 @@
 class CreateOrganisations < ActiveRecord::Migration
   def change
     create_table :organisations do |t|
+      t.integer :invoices_left, :default => 0
+      t.integer :receipts_left, :default => 0
       t.string :name
       t.string :subdomain
 
