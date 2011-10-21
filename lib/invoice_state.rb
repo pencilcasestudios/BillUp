98bf2ac6fdb21827845f5a13ee15955c980dd69e
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-10-21 17:36:13 +0200

    Updated lib/invoices_states.rb

--- lib/invoice_state.rb
+++ lib/invoice_state.rb
@@ -1,5 +1,5 @@
 class InvoiceState < ActiveRecord::Base
-  NAME = {
+  STATES = {
     # Translation                                                             # Database key
     I18n.t("views.invoices._form.fields.invoice")                =>           "invoice",
     I18n.t("views.invoices._form.fields.quote")                =>             "quote",
