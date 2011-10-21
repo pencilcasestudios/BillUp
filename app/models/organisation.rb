1b276d9b7caa8ed1e218c40174e6fcabb8f1776a
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-10-21 17:34:10 +0200

    invoices_left and receipts_left cannot fall below 0.

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,22 +1,23 @@
 class Organisation < ActiveRecord::Base
   default_scope order("name")
-  
-  validates :invoices_left, :presence => true, :numericality => true
-  validates :name, :presence => true
-  validates :preferred_due_in_period, :presence => true, :numericality => { :greater_than => 0 }
-  validates :receipts_left, :presence => true, :numericality => true
-  validates :subdomain, :presence => true, :uniqueness => true, :subdomain_format => true
 
-  has_many :addresses, :as => :addressable, :dependent => :destroy
+  has_paper_trail
+
+  validates :invoices_left, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :preferred_due_in_period, presence: true, numericality: { greater_than: 0 }
+  validates :receipts_left, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :subdomain, presence: true, uniqueness: true, subdomain_format: true
+
+  has_many :addresses, as: :addressable, dependent: :destroy
   has_many :clients
   has_many :invoices
+  has_many :members, through: :memberships, source: :user
   has_many :memberships
-  has_many :members, :through => :memberships, :source => :user
 
-  accepts_nested_attributes_for :addresses, :allow_destroy => true
+  accepts_nested_attributes_for :addresses, allow_destroy: true
 
   before_save :downcase_subdomain
-
 
   def current_address
     "Current Address"
