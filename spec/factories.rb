9e709552b56bdd9dd7e9f7d501ff7a10ba324653
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-10-22 10:37:39 +0200

    Updated factories. Still have an error generating an Invoice from Factory Girl.

--- spec/factories.rb
+++ spec/factories.rb
@@ -13,6 +13,25 @@ end
 
 
 
+Factory.define :address do |f|
+  f.sequence(:country) { |n| "Country #{n}" }
+  f.sequence(:email) { |n| "email#{n}@example.com" }
+  f.sequence(:label) { |n| "Label #{n}" }
+  f.sequence(:postal_code) { |n| "Postal Code #{n}" }
+  f.sequence(:province) { |n| "Province #{n}" }
+  f.sequence(:street) { |n| "Street #{n}" }
+  f.sequence(:town) { |n| "Town #{n}" }
+  f.sequence(:website) { |n| "http://website#{n}.example.com" }
+
+  f.cell_phone_number [["+",""][rand 2],"#{'%010d' % (rand 1000000000000)}"].join.strip
+  f.fax [["+",""][rand 2],"#{'%010d' % (rand 1000000000000)}"].join.strip
+  f.land_line_number [["+",""][rand 2],"#{'%010d' % (rand 1000000000000)}"].join.strip
+end
+
+
+
+
+
 Factory.define :organisation do |f|
   f.sequence(:name) { |n| "Organisation #{n}" }
   f.sequence(:subdomain) { |n| "subdomain#{n}" }
@@ -40,24 +59,6 @@ Factory.define :invoice do |f|
 
   f.uuid Time.now.strftime("%s")
   f.state InvoiceState::STATES[InvoiceState::STATES.to_a[rand InvoiceState::STATES.size].first]
-end
-
-
-
-
-Factory.define :address do |f|
-  f.sequence(:country) { |n| "Country #{n}" }
-  f.sequence(:email) { |n| "email#{n}@example.com" }
-  f.sequence(:label) { |n| "Label #{n}" }
-  f.sequence(:postal_code) { |n| "Postal Code #{n}" }
-  f.sequence(:province) { |n| "Province #{n}" }
-  f.sequence(:street) { |n| "Street #{n}" }
-  f.sequence(:town) { |n| "Town #{n}" }
-  f.sequence(:website) { |n| "http://website#{n}.example.com" }
-
-  f.cell_phone_number [["+",""][rand 2],"#{'%010d' % (rand 1000000000000)}"].join.strip
-  f.fax [["+",""][rand 2],"#{'%010d' % (rand 1000000000000)}"].join.strip
-  f.land_line_number [["+",""][rand 2],"#{'%010d' % (rand 1000000000000)}"].join.strip
 end
 
 
