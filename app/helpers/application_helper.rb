64de9c6fe464c42e37f205ad1d6bbb0b3be5d744
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-10-24 07:40:16 +0200

    Invoices can now be edited.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,11 @@ module ApplicationHelper
 
   def new_or_create_action?
     (controller.action_name == "new") || (controller.action_name == "create")
-  end 
+  end
+
+  def edit_or_update_action?
+    (controller.action_name == "edit") || (controller.action_name == "update")
+  end
 
   def title(page_title)
     content_for(:title) { page_title }
