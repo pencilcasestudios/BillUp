9668dacf29e496774126f9dffb43be574377a4d4
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-10-25 06:21:12 +0200

    Updated the example deployment recipe.

--- config/examples/deploy.rb
+++ config/examples/deploy.rb
@@ -1,22 +1,78 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# Deployment reference documentation:
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# Ref: http://beginrescueend.com/integration/capistrano/
+# http://thoughtsincomputation.com/posts/deploying-in-harmony-capistrano-rvm-bundler-and-git
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))              # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                                            # Load RVM's capistrano plugin.
+# Don't forget to create gemset on the server
+set :rvm_ruby_string, "ruby-1.9.2-p290@billup-rails.3.1.x"          # Select the gemset
+set :rvm_type, :user                                                # RVM install is in the deploying user's home directory
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+require "bundler/capistrano"
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+
+
+depend :remote, :gem, "bundler", ">=1.0.21"
+depend :remote, :gem, "rake", ">=0.9.2.2"
+
+
+
+
+
+set :application, "billup.net"
+role :app, application
+role :web, application
+role :db,  application, :primary => true
+
+
+
+
+set :user, "silumesii"
+set :deploy_to, "/var/Apps/BillUp"
+set :deploy_via, :remote_cache
+set :copy_cache, true
+set :use_sudo, false
+
+
+
+
+set :scm, :git
+# Don't forget to make this repo on the server
+set :repository, "silumesii@billup.net:/var/Repositories/Git/BillUp.git"
+# Don't forget to make this branch in the repository
+set :branch, "deployment"
+
+
+
+
+namespace :deploy do
+  task :start do ; end
+
+  task :stop do ; end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,"tmp","restart.txt")}"
+  end
+
+  desc "Setup shared directory."
+  task :setup_shared do
+    run "mkdir #{shared_path}/config"
+    put File.read("config/examples/database.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/examples/config.yml"), "#{shared_path}/config/config.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+
+  desc "Symlink extra configs and folders."
+  task :symlink_extras do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+  end
+end
+
+
+
+
+after "deploy", "deploy:cleanup" # keeps only last 5 releases
+after "deploy:setup", "deploy:setup_shared"
+after "deploy:update_code", "deploy:symlink_extras"
