b53b8170891e9d345124d0f2f583a0e65150c089
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-10-25 06:51:41 +0200

    Notes for Capistrano deployment.

--- config/examples/deploy.rb
+++ config/examples/deploy.rb
@@ -1,10 +1,29 @@
 # Deployment reference documentation:
 # https://github.com/capistrano/capistrano/wiki/2.x-From-The-Beginning
 
-# Basic steps
-# cap deploy:setup
-# cap deploy:check
-# cap deploy:update - May require verifying the host key
+# Basic steps to setup
+# ON THE SERVER
+
+# Create the gemset:
+# $ rvm gemset create ruby-1.9.2-p290@billup-rails.3.1.x
+
+# Install rake and bundler
+# $ rvm use ruby-1.9.2-p290@billup-rails.3.1.x
+# $ gem install bunder rake
+
+# Create the repository:
+# $ mkdir /var/Repositories/Git/BillUp.git
+# $ cd /var/Repositories/Git/BillUp.git
+# $ git init --bare
+
+# Create the remotes in the development repository
+# $ git remote add deployment silumesii@billup.net:/var/Repositories/Git/BillUp.git
+
+# ON THE DEVELOPMENT MACHINE
+# git push deployment deployment
+# $ cap deploy:setup
+# $ cap deploy:check
+# $ cap deploy:update - May require verifying the host key
 
 # Ref: http://beginrescueend.com/integration/capistrano/
 # http://thoughtsincomputation.com/posts/deploying-in-harmony-capistrano-rvm-bundler-and-git
