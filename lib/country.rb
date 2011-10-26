bc25d5f036a2ba53e8e517ffc32f06fcd68c329c
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-10-26 06:14:59 +0200

    Added 'Canada' to Country list.

--- lib/country.rb
+++ lib/country.rb
@@ -1,6 +1,7 @@
 class Country < ActiveRecord::Base
   NAMES = {
     # Translation                                                             # Database key
+    I18n.t("models.country.names.canada")                =>                   "Canada",
     I18n.t("models.country.names.zambia")                =>                   "Zambia",
   }
 end
