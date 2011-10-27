d8fedc608de248b1eb6232305b0a7008ad049c22
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-10-27 16:20:43 +0200

    Added 'Tanzania' to countries.

--- lib/country.rb
+++ lib/country.rb
@@ -1,8 +1,9 @@
 class Country < ActiveRecord::Base
   NAMES = {
-    # Translation                                               # Database key
+    # Translation                                                   # Database key
     I18n.t("models.country.names.canada")      =>                   "Canada",
-    I18n.t("models.country.names.netherlands") =>              "Netherlands",
+    I18n.t("models.country.names.netherlands") =>                   "Netherlands",
+    I18n.t("models.country.names.tanzania")    =>                   "Tanzania",
     I18n.t("models.country.names.zambia")      =>                   "Zambia",
   }
 end
