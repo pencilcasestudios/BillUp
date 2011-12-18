0a5e1b91b1f1424787ad6fab1ffe7a0c490c75ec
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-18 12:08:04 +0200

    delayed_job tables.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111108074204) do
+ActiveRecord::Schema.define(:version => 20111217100329) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "addressable_id"
@@ -41,6 +41,21 @@ ActiveRecord::Schema.define(:version => 20111108074204) do
   end
 
   add_index "clients", ["organisation_id"], :name => "index_clients_on_organisation_id"
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "invoices", :force => true do |t|
     t.datetime "due_at"
