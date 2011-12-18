9d29e0f0fbe45d0fade5cac96f856301972fbcc3
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2011-12-18 16:36:06 +0200

    Deliver the registration confirmation email in the background.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  before_filter :sign_in_required, :except => [:new, :create]
+  before_filter :sign_in_required, except: [:new, :create]
 
   def index
-    redirect_to root_url(:host => request.domain)
+    redirect_to root_url(host: request.domain)
   end
 
   def new
     if current_user
-      flash[:notice] = t('controllers.users_controller.actions.new.error')
-      redirect_to root_url(:host => request.domain)
+      flash[:notice] = t("controllers.users_controller.actions.new.error")
+      redirect_to root_url(host: request.domain)
     else
       @user = User.new
     end
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      Emailer.registration_confirmation(@user).deliver  
-      flash[:success] = t('controllers.users_controller.actions.create.success')
+      #Emailer.registration_confirmation(@user).deliver  
+      Emailer.delay.registration_confirmation(@user)
+      flash[:success] = t("controllers.users_controller.actions.create.success")
       redirect_to sign_in_path
     else
       render action: "new"
@@ -32,9 +33,9 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update_attributes(params[:user])
-      redirect_to(account_settings_path, :notice => t('controllers.users_controller.actions.update.success'))
+      redirect_to(account_settings_path, notice: t("controllers.users_controller.actions.update.success"))
     else
-      render :action => "edit"
+      render action: "edit"
     end
   end
 end
