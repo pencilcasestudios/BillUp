9680470f6948b83fa5e426295c254491d8bb5abe
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-01-22 18:11:48 +0200

    Updated the exception_notification setup. Ref: https://github.com/smartinez87/exception_notification/issues/53#issuecomment-3499016.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,3 +58,9 @@ BillUp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
+
+# Ref: http://stackoverflow.com/questions/3524127/exception-notification-gem-and-rails-3
+BillUp::Application.config.middleware.use ExceptionNotifier,
+  :email_prefix => "[BillUp Exception] ",
+  :sender_address => %{"BillUp Exceptions Notifier" <exceptions+no-reply@billup.net>},
+  :exception_recipients => %w{exceptions_group@billup.net}
--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,5 +1,5 @@
-# Ref: http://stackoverflow.com/questions/3524127/exception-notification-gem-and-rails-3
-Rails.application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[BillUp Exception] ",
-  :sender_address => %{"BillUp Exceptions Notifier" <exceptions+no-reply@billup.net>},
-  :exception_recipients => %w{exceptions_group@billup.net}
+## Ref: http://stackoverflow.com/questions/3524127/exception-notification-gem-and-rails-3
+#Rails.application.config.middleware.use ExceptionNotifier,
+#  :email_prefix => "[BillUp Exception] ",
+#  :sender_address => %{"BillUp Exceptions Notifier" <exceptions+no-reply@billup.net>},
+#  :exception_recipients => %w{exceptions_group@billup.net}
