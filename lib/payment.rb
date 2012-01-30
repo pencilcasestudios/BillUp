d40bd6f034600b4e802edc00d355745335f6ecdc
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-01-30 10:36:05 +0200

    Cash deposit as a payment option.

--- lib/payment.rb
+++ lib/payment.rb
@@ -4,6 +4,7 @@ class Payment < ActiveRecord::Base
     I18n.t("models.payment.types.airtel_money")  =>                   "airtel_money",
     I18n.t("models.payment.types.bank_transfer") =>                   "bank_transfer",
     I18n.t("models.payment.types.cash")          =>                   "cash",
+    I18n.t("models.payment.types.cash_deposit")          =>           "cash_deposit",
     I18n.t("models.payment.types.cheque")        =>                   "cheque",
     I18n.t("models.payment.types.debit_card")    =>                   "debit_card",
     I18n.t("models.payment.types.mtn_money")     =>                   "mtn_money",
