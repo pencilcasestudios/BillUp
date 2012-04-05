57f00fda5bcea66bf9bafed457fc1b7ea13ae32c
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-04-05 10:16:34 +0200

    Updated factories to work with the updated factory girl.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,4 @@
 FactoryGirl.define do
-  factory :user do
-    sequence(:email) { |n| "grace#{n}@example.com" }
-    sequence(:username) { |n| "grace#{n}" }
-    sequence(:cell_phone_number) { PhoneNumber.random }
-    
-    name "Grace Nyambe"
-    language "eng"
-    password AppConfig.test_user_password
-    time_zone "Harare"
-  end
-
   factory :address do
     sequence(:cell_phone_number) { PhoneNumber.random }
     sequence(:country) { |n| "Country #{n}" }
@@ -23,14 +12,7 @@ FactoryGirl.define do
     sequence(:town) { |n| "Town #{n}" }
     sequence(:website) { |n| "http://website#{n}.example.com" }
   end
-
-  factory :organisation do
-    sequence(:name) { |n| "Organisation #{n}" }
-    sequence(:subdomain) { |n| "subdomain#{n}" }
-
-    addresses { |a| [a.association(:address)] }
-  end
-
+  
   factory :client do
     sequence(:name) { |n| "Client #{n}" }
     sequence(:organisation_id) { |n| "#{n}" }
@@ -38,7 +20,7 @@ FactoryGirl.define do
     association :organisation
     addresses { |a| [a.association(:address)] }
   end
-
+  
   factory :invoice do
     sequence(:description) { |n| "Description #{n}" }
     sequence(:invoice_number) { |n| n }
@@ -48,8 +30,15 @@ FactoryGirl.define do
     from_address_id { |c| c.client.id }
     to_address_id { |o| o.organisation.id }
 
-    uuid Time.now.strftime("%s")
-    state InvoiceState::STATES[InvoiceState::STATES.to_a[rand InvoiceState::STATES.size].first]
+    sequence(:uuid) { SecureRandom.uuid }
+    sequence(:state) { InvoiceState::STATES[InvoiceState::STATES.to_a[rand InvoiceState::STATES.size].first] }
+  end
+
+  factory :organisation do
+    sequence(:name) { |n| "Organisation #{n}" }
+    sequence(:subdomain) { |n| "subdomain#{n}" }
+
+    addresses { |a| [a.association(:address)] }
   end
 
   factory :receipt do
@@ -62,9 +51,20 @@ FactoryGirl.define do
     from_address_id { |o| o.organisation.id }
     to_address_id { |c| c.client.id }
 
-    currency Currency::CODES[Currency::CODES.to_a[rand Currency::CODES.size].first]
-    payment_method Payment::TYPES[Payment::TYPES.to_a[rand Payment::TYPES.size].first]
-    received_at Time.now
-    uuid Time.now.strftime("%s")
+    sequence(:currency) { Currency::CODES[Currency::CODES.to_a[rand Currency::CODES.size].first] }
+    sequence(:payment_method) { Payment::TYPES[Payment::TYPES.to_a[rand Payment::TYPES.size].first] }
+    sequence(:received_at) { Time.now }
+    sequence(:uuid) { SecureRandom.uuid }
   end
+  
+  factory :user do
+    sequence(:email) { |n| "grace#{n}@example.com" }
+    sequence(:username) { |n| "grace#{n}" }
+    sequence(:cell_phone_number) { PhoneNumber.random }
+
+    name "Grace Nyambe"
+    language "eng"
+    password AppConfig.test_user_password
+    time_zone "Harare"
+  end  
 end
