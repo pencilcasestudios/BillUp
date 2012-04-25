1a659088c0f429b2701fc7b26f1d63469e97e378
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-04-25 19:49:05 +0200

    Corrected the scope on invoices for paid invoices.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ActiveRecord::Base
   default_scope order("invoice_number DESC")
   
-  scope :paid, where(paid_at: !nil)
+  scope :paid, where("paid_at is not null")
   scope :unpaid, where(paid_at: nil)
   
   has_paper_trail
