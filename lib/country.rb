888837f9004cb220349d5140d5d7126225c00331
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-06-01 06:44:12 +0200

    Updated bundled gems. Added South Africa as a currency and a country.

--- lib/country.rb
+++ lib/country.rb
@@ -1,10 +1,10 @@
 class Country < ActiveRecord::Base
   COUNTRIES = {
-    # Translation                                                   # Database key
-    I18n.t("models.country.countries.canada")      =>               "Canada",
-    I18n.t("models.country.countries.netherlands") =>               "Netherlands",
-    I18n.t("models.country.countries.tanzania")    =>               "Tanzania",
-    I18n.t("models.country.countries.zambia")      =>               "Zambia",
+    I18n.t("models.country.countries.canada")       =>               "Canada",
+    I18n.t("models.country.countries.netherlands")  =>               "Netherlands",
+    I18n.t("models.country.countries.south_africa") =>               "South Africa",
+    I18n.t("models.country.countries.tanzania")     =>               "Tanzania",
+    I18n.t("models.country.countries.zambia")       =>               "Zambia",
   }
 
   def self.random_country
--- lib/currency.rb
+++ lib/currency.rb
@@ -1,10 +1,11 @@
 class Currency < ActiveRecord::Base
   CODES = {
-    I18n.t("models.currency.codes.zmk") => "zmk",
-    I18n.t("models.currency.codes.usd") => "usd",
     I18n.t("models.currency.codes.cad") => "cad",
-    I18n.t("models.currency.codes.gbp") => "gbp",
     I18n.t("models.currency.codes.eur") => "eur",
+    I18n.t("models.currency.codes.gbp") => "gbp",
+    I18n.t("models.currency.codes.usd") => "usd",
+    I18n.t("models.currency.codes.zar") => "zar",
+    I18n.t("models.currency.codes.zmk") => "zmk",
   }
   
   def self.random_currency_code
