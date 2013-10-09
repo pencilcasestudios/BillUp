571e65288e2f14ad61936e26178091841a962676
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-10-09 08:45:24 +0200

    Updated Exception Notification syntax. Ref: https://github.com/smartinez87/exception_notification/issues/166.

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,5 +1,7 @@
 # Ref: http://stackoverflow.com/questions/3524127/exception-notification-gem-and-rails-3
-Rails.application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[BillUp Exception] ",
-  :sender_address => %{"BillUp Exceptions Notifier" <exceptions+no-reply@billup.net>},
-  :exception_recipients => %w{exceptions_group@billup.net}
+# Ref: https://github.com/smartinez87/exception_notification
+Rails.application.config.middleware.use ExceptionNotification::Rack, :email => {
+	:email_prefix => "[BillUp Exception] ",
+	:sender_address => %{"BillUp Exceptions Notifier" <exceptions+no-reply@billup.net>},
+	:exception_recipients => %w{exceptions_group@billup.net}
+}
