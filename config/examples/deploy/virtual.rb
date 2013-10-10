5c7faf34928b86c23304318e8babe9290cee584f
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-10-10 12:51:50 +0200

    Preparing for multistage deployment with a virtual machine as default.

--- config/examples/deploy/virtual.rb
+++ config/examples/deploy/virtual.rb
@@ -1,11 +1,11 @@
 # Ref: http://gist.github.com/293302
-server DEPLOYMENT_CONFIG["staging_server_name"], :web, :app, :db, primary: true
+server DEPLOYMENT_CONFIG["virtual_server_name"], :web, :app, :db, primary: true
 
-set :deployment_path, DEPLOYMENT_CONFIG["staging_deployment_path"]
+set :deployment_path, DEPLOYMENT_CONFIG["virtual_deployment_path"]
 set :deploy_to, "/var/Apps/#{application}/#{deployment_path}"
 
 # Don't forget to make this repo on the server
-set :repository_server_name, DEPLOYMENT_CONFIG["staging_server_name"]
+set :repository_server_name, DEPLOYMENT_CONFIG["virtual_server_name"]
 set :repository, "#{user}@#{repository_server_name}:/var/Repositories/Git/#{application}.git"
 # Don't forget to make this branch in the repository
-set :branch, DEPLOYMENT_CONFIG["staging_repository_deployment_branch"]
+set :branch, DEPLOYMENT_CONFIG["virtual_repository_deployment_branch"]
